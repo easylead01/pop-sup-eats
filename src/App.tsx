85e06cde7c5c70995e6751fbe393a9c7b2abc070
easylead01 <hatiko07@gmail.com>
2025-11-30 16:36:14 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,35 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { ProductImageProvider } from "@/components/ProductImageProvider";
+
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ProductImageProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/admin" element={<Admin />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </HashRouter>
-      </TooltipProvider>
-    </ProductImageProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ProductImageProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/admin" element={<Admin />} />
+
+              {/* 404 для любых НЕсовпадающих путей */}
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </HashRouter>
+
+        </TooltipProvider>
+      </ProductImageProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
